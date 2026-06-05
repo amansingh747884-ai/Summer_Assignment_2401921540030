2611cd928bcf4a97ad62840bb21bef796ccf35e1
Aman Singh <amansingh747884@gmail.com>
2026-06-05 11:28:25 +0530

    week 1 day 5 solutions

---
+++ Week_1/Day_5/ReverseString.java
@@ -0,0 +1,17 @@
+package Week_1.Day_5;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
+
