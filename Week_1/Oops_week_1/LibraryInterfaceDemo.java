22a292bc8094c3f59893adde2bf94cd5bb15e013
Aman Singh <amansingh747884@gmail.com>
2026-06-07 15:42:09 +0530

    week 1 opps solution

---
+++ Week_1/Oops_week_1/LibraryInterfaceDemo.java
@@ -0,0 +1,32 @@
+package Week_1.Oops_week_1;
+
+public class LibraryInterfaceDemo {
+    public static void main(String[] args) {
+        
+        // Test Case #1: KidUser
+        KidUser kid1 = new KidUser();
+        kid1.age = 10;
+        kid1.registerAccount();
+        kid1.bookType = "Kids";
+        kid1.requestBook();
+
+        KidUser kid2 = new KidUser();
+        kid2.age = 18;
+        kid2.registerAccount();
+        kid2.bookType = "Fiction";
+        kid2.requestBook();
+
+        // Test Case #2: AdultUser
+        AdultUser adult1 = new AdultUser();
+        adult1.age = 5;
+        adult1.registerAccount();
+        adult1.bookType = "Kids";
+        adult1.requestBook();
+
+        AdultUser adult2 = new AdultUser();
+        adult2.age = 23;
+        adult2.registerAccount();
+        adult2.bookType = "Fiction";
+        adult2.requestBook();
+    }
+}
---
+++ Week_1/Oops_week_1/LibraryUser.java
@@ -0,0 +1,6 @@
+package Week_1.Oops_week_1;
+
+public interface LibraryUser {
+    void registerAccount();
+    void requestBook();
+}
