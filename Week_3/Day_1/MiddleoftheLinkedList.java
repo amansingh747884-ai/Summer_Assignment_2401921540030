9a5107e580ced0271d0abb34c936bc4c8ae2ba04
Aman Singh <amansingh747884@gmail.com>
2026-06-15 13:37:29 +0530

    week3 day1 solutions

---
+++ Week_3/Day_1/MiddleoftheLinkedList.java
@@ -0,0 +1,24 @@
+package Week_3.Day_1;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class MiddleoftheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
---
+++ Week_3/Day_1/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+package Week_3.Day_1;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+        while (temp != null) {
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}
