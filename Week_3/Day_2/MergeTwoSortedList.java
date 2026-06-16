a5c5428593fadaf80ef8ecc64a7f0560532d410d
Aman Singh <amansingh747884@gmail.com>
2026-06-16 17:46:17 +0530

    week3 day 2 solutions

---
+++ Week_3/Day_2/MergeTwoSortedList.java
@@ -0,0 +1,35 @@
+package Week_3.Day_2;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val < temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 == null)
+            temp.next = temp2;
+        else
+            temp.next = temp1;
+        return head.next;
+    }
+}
---
+++ Week_3/Day_2/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,31 @@
+package Week_3.Day_2;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class RemoveNthNodeFromEndofList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
